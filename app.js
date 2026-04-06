87f7d36e7329de16b9d202749584aa07ebd6d71b
matias nazal <matiasnazal@soclifar.cl>
2026-04-06 14:50:47 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({ message: "Microservicio Operativo - Despliegue Exitoso", status: "OK" });
+});
+
+// Endpoint para simular una feature
+app.get('/version', (req, res) => {
+    res.json({ version: "1.0.0", env: "Development" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
